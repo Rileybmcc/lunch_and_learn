f0a409c9776ee01d8c8b924dcf49ee136ec2e261
Riley McCullough <rileybmccullough@gmail.com>
2022-11-15 16:49:24 -0700

    Saving progress to switch branch

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -2,15 +2,21 @@ class YoutubeService
 
   def self.conn
     Faraday.new(url: "https://www.youtube.googleapis.com/youtube/v3/") do |faraday|
-      faraday.params['key'] = ENV['ytube_key']
+      faraday.params['key'] = ENV["ytube_key"]
       faraday.params['part'] = "snippet"
       faraday.params['channelId'] = "UCluQ5yInbeAkkeCndNnUhpw"
     end
   end
 
   def self.vids_about_country(country)
-    # require "pry"; binding.pry
-    response = conn.get("search?q=#{country}")
+
+# require "pry"; binding.pry
+
+    # response = conn.get("search?q=#{country}")
+    response = conn.get("search?q=#{country}") do |request|
+      request.params['country'] = country
+    end
+
     JSON.parse(response.body, symbolize_names: true)
   end
 
